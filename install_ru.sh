1b29ff50be87d905c0b7d6721c6e5db880a92118
zombie665 <83660122+zombie665@users.noreply.github.com>
2026-02-06 22:15:58 +0500

    Update install_ru.sh

--- install_ru.sh
+++ install_ru.sh
@@ -2166,25 +2166,25 @@ remove_node_port() {
 # ============================================
 # Warp install
 warp_install{} {
-    echo "\e[38;2;0;255;255m--- 1. Установка Cloudflare WARP ---\e[0m"
+    echo -e "\e[38;2;0;255;255m--- 1. Установка Cloudflare WARP ---\e[0m"
 curl -fsSL https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt update && sudo apt install cloudflare-warp -y
 
-echo "\e[38;2;0;255;255m--- 2. Настройка WARP в режиме Proxy ---\e[0m"
+echo -e "\e[38;2;0;255;255m--- 2. Настройка WARP в режиме Proxy ---\e[0m"
 warp-cli registration new
 warp-cli mode proxy
 warp-cli proxy port 4000
 warp-cli connect
 
-echo "\e[38;2;0;255;255m--- 3. Установка GOST (входной мост) ---\e[0m"
+echo -e "\e[38;2;0;255;255m--- 3. Установка GOST (входной мост) ---\e[0m"
 GOST_VERSION="3.0.0-rc10"
 wget https://github.com/go-gost/gost/releases/download/v${GOST_VERSION}/gost_${GOST_VERSION}_linux_amd64.tar.gz
 tar -xvzf gost_${GOST_VERSION}_linux_amd64.tar.gz
 sudo mv gost /usr/bin/
 rm gost_${GOST_VERSION}_linux_amd64.tar.gz README* LICENSE*
 
-echo "\e[38;2;0;255;255m--- 4. Создание службы для автозапуска GOST ---\e[0m"
+echo -e "\e[38;2;0;255;255m--- 4. Создание службы для автозапуска GOST ---\e[0m"
 sudo tee /etc/systemd/system/gost.service > /dev/null <<EOF
 [Unit]
 Description=Gost Proxy Bridge
@@ -2202,7 +2202,7 @@ EOF
 while true; do
 	read -p "Укажите логин для внешнего прокси: " login
 	if [ -z "$login" ]; then
-		echo "\e[91mНе может быть пустым!\e[0m"
+		echo -e "\e[91mНе может быть пустым!\e[0m"
 	continue
 	fi
 	break
@@ -2211,7 +2211,7 @@ sed -i 's/PROXY_USER/'$login'/g' /etc/systemd/system/gost.service
 while true; do
         read -p "Укажите пароль для внешнего прокси: " password
 	if [ -z "$password" ]; then
-		echo "\e[91mНе может быть пустым!\e[0m"
+		echo -e "\e[91mНе может быть пустым!\e[0m"
 	continue
 	fi
 	break
@@ -2220,7 +2220,7 @@ sed -i 's/PROXY_PASS/'$password'/g' /etc/systemd/system/gost.service
 while true; do
 	read -p "Укажите внешний порт прокси: " port
 	if [ -z "$port" ]; then
-		echo "\e[91mНе может быть пустым!\e[0m"
+		echo -e "\e[91mНе может быть пустым!\e[0m"
 	continue
 	fi
 	break
@@ -2229,13 +2229,13 @@ sed -i 's/PROXY_PORT/'$port'/g' /etc/systemd/system/gost.service /etc/systemd/sy
 sudo systemctl daemon-reload
 sudo systemctl enable --now gost
 
-echo "		\e[38;2;0;255;255m--- Успех! ---\e[0m"
+echo -e "		\e[38;2;0;255;255m--- Успех! ---\e[0m"
 sleep 5
 clear
 ss -tlnp | grep 4000
 curl -x socks5h://127.0.0.1:4000 https://www.cloudflare.com/cdn-cgi/trace
 
-    echo "\e[38;2;0;255;255m"
+    echo -e "\e[38;2;0;255;255m"
     echo "-----------------------------------------------------------------"
     echo "			ГОТОВО!"
     echo "	Ваш внешний прокси: SOCKS5 или HTTP"
@@ -2247,6 +2247,7 @@ curl -x socks5h://127.0.0.1:4000 https://www.cloudflare.com/cdn-cgi/trace
     echo "-----------------------------------------------------------------"
     echo "\e[0m"
 }
+
 # Save configuration
 save_config() {
     local panel_port="$1"
